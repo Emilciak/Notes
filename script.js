4cf3c193d63e4a2defbaf3c52102a0a3183757d7
Emilciak <emilialepak@gmail.com>
2023-03-27 17:53:47 +0200

    made function to delete notes

--- script.js
+++ script.js
@@ -10,3 +10,79 @@ const textArea = document.querySelector('#text')
 const saveNoteBtn = document.querySelector('.save')
 const cancelNoteBtn = document.querySelector('.cancel')
 const error = document.querySelector('.error')
+
+let selValue
+let cardId = 0
+const showAddNote = () => {
+	noteAdd.style.display = 'flex'
+}
+const closeAddNote = () => {
+	noteAdd.style.display = 'none'
+	error.style.visibility = 'hidden'
+	textArea.value = ''
+	category.selectedIndex = 0
+}
+
+const addNote = () => {
+	if (textArea.value !== '' && category.options[category.selectedIndex].value !== '0') {
+		createNote()
+		error.style.visibility = 'hidden'
+	} else {
+		error.style.visibility = 'visible'
+	}
+}
+
+const createNote = () => {
+	const newNote = document.createElement('div')
+	newNote.classList.add('note')
+	newNote.setAttribute('id', cardId)
+	const newHeader = document.createElement('div')
+	newNote.innerHTML = `
+    <div class="note-header">
+    <h3 class="note-title">${selValue}</h3>
+    <button class="delete-note" onclick=deleteNote(${cardId})>
+        <i class="fas fa-times icon"></i>
+    </button>
+</div>
+<div class="node-body">
+   ${textArea.value}
+</div>
+    `
+	noteAdd.style.display = 'none'
+    color(newNote)
+	areaNote.appendChild(newNote)
+    textArea.value=''
+    category.selectedIndex = 0
+	cardId++
+}
+
+const selectValue = () => {
+	selValue = category.options[category.selectedIndex].text
+}
+
+const color = note =>{
+    switch(selValue){
+        case "Shopping":
+            note.style.backgroundColor = "#a2d2ff"
+            break;
+        case "Work":
+            note.style.backgroundColor = "#ffafcc"
+            break;
+        case "Other":
+            note.style.backgroundColor = "#ccd5ae"
+    }
+}
+
+const deleteNote = (id) =>{
+ const noteToDel = document.getElementById(id)
+ areaNote.removeChild(noteToDel )
+}
+
+const deleteAll = () =>{
+areaNote.textContent=""
+}
+addBtn.addEventListener('click', showAddNote)
+cancelNoteBtn.addEventListener('click', closeAddNote)
+saveNoteBtn.addEventListener('click', addNote)
+deleteAllBtn.addEventListener('click', deleteAll)
+
