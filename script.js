8e4df8dab19b9b5e8ce738c4df3ed10678dc180f
Emilia Lepak <emilialepak2000@gmail.com>
2023-03-27 16:04:36 +0200

    js started

---
+++ script.js
@@ -0,0 +1,12 @@
+const addBtn = document.querySelector('.add')
+const deleteAllBtn = document.querySelector('.delete')
+
+const areaNote = document.querySelector('.area-note')
+const deleteBtns = document.getElementsByClassName('.delete-note')
+
+const noteAdd = document.querySelector('.node-adding')
+const category = document.querySelector('#category')
+const textArea = document.querySelector('#text')
+const saveNoteBtn = document.querySelector('.save')
+const cancelNoteBtn = document.querySelector('.cancel')
+const error = document.querySelector('.error')
